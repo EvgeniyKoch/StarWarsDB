77c1502e38d0bfddce98cf64dec953c71b66e1f3
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-02-06 10:48:47 +0300

    Updates

---
+++ precache-manifest.a7df2c5e8ed163c56e5fdc41018a065f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9d155c1563a8a449bea",
+    "url": "/StarWarsDB/static/js/runtime~main.f9d155c1.js"
+  },
+  {
+    "revision": "c836c091281231703f40",
+    "url": "/StarWarsDB/static/js/main.c836c091.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/StarWarsDB/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "c836c091281231703f40",
+    "url": "/StarWarsDB/static/css/main.a55b6b43.chunk.css"
+  },
+  {
+    "revision": "a2513accd659f3a74b24380badaac1d0",
+    "url": "/StarWarsDB/index.html"
+  }
+];
